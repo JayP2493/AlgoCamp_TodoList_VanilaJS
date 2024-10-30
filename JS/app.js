3924325da3e2ab179c6aa91660c659505bceace7
jay1patel1 <jayp.fsd@outlook.com>
2024-10-30 21:23:44 +0530

    input text updated

--- JS/app.js
+++ JS/app.js
@@ -5,19 +5,39 @@ function loadTodos () {
     if (!todosCheck) {
         console.log(todosCheck, "No value is there, created new");
         localStorage.clear();
-        localStorage.setItem("todos", JSON.stringify({todos: []}));
+        localStorage.setItem("todos", JSON.stringify({todoList: []}));
     }
+    return JSON.parse(localStorage.getItem("todos"));
+}
 
-    const todos = JSON.parse(localStorage.getItem("todos"));
+function addTodoToLocalStorage(todoText) {
+    const todos = loadTodos().todoList;
+    if (!(todos.length !== 0 && todos[todos.length-1] !== todoText)) {
+        todos.push(todoText);
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }
     console.log(todos);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const todoInput = document.getElementById("todoInput");
 
-    todoInput.addEventListener("change", () => {
-        console.log("Something Changed")
+document.addEventListener("DOMContentLoaded", function() {
+    loadTodos();
+    const todoInput = document.getElementById("todoInput");
+    const submitButton = document.getElementById("addTodo");
+
+    submitButton.addEventListener("click", (event ) => {
+        const todoText = todoInput.value;
+        if (todoText === '') {
+            alert("Please write something in ToDo text area");
+        } else {
+            addTodoToLocalStorage(todoText);
+        }
+
     })
 
-    loadTodos();
+    todoInput.addEventListener("change", (event) => {
+        const todoText = event.target.value;
+        event.target.value = todoText.trim();
+        console.log(event.target.value);
+    })
 })
