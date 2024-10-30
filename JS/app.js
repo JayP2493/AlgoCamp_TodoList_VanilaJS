86b92908138f2d35ba6f66862c99bb1fa18800eb
jay1patel1 <jayp.fsd@outlook.com>
2024-10-30 17:47:18 +0530

    playing with local memory

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+function loadTodos () {
+//     This function will get todos from browser memory
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    console.log(todos);
+
+}
+document.addEventListener("DOMContentLoaded", function() {})
