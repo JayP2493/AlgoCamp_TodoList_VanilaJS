4f7ee19561a8417a660e42719c908ee32ce8ea8a
jay1patel1 <jayp.fsd@outlook.com>
2024-10-30 18:12:10 +0530

    Todo Array creation done

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,17 @@
 function loadTodos () {
-//     This function will get todos from browser memory
+//     This function will get todos from browser memory if not it will create the one
+    const todosCheck = JSON.parse(localStorage.getItem("todos"));
+
+    if (!todosCheck) {
+        localStorage.clear();
+        localStorage.setItem("todos", JSON.stringify({todos: []}));
+    }
+
     const todos = JSON.parse(localStorage.getItem("todos"));
     console.log(todos);
 
 }
-document.addEventListener("DOMContentLoaded", function() {})
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadTodos();
+})
